3e9e81242a4077240f2799a30e384e85fae51574
devanshi_kulshreshtha <92775842+devaanshiiiiiiiii@users.noreply.github.com>
2024-09-25 15:53:06 +0530

    Update program1.py
    i have gone through the approach of stack and by check each value and comparing  it we get the solution of problem statment .. my approach is when in a string my ittrator i found a closing parenthesis then it checks for opening , if found then TRUE else False

--- program1.py
+++ program1.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        Stack=[]
+        DataBrackets={'[':']','{':'}','(':')'}
+        for i in s:
+            if i in DataBrackets.values():
+                Stack.append(i)
+            elif i in DataBrackets.values():
+                if Stack and Stack[-1]== DataBrackets[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                return False 
+        return not Stack
+        
 
 
 
