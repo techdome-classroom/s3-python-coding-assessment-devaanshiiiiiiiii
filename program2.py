388c00139555d924d4c186c7912f646954ded9da
devanshi_kulshreshtha <92775842+devaanshiiiiiiiii@users.noreply.github.com>
2024-09-25 16:04:24 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,9 +1,18 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        Roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        Valid_combination=['IV','IX','XL','XC','CD','CM']
+        val=0
+        for i in range(len(Roman)):
+            if Roman[i] not in Roman:
+                return False 
+            if i>0  and Roman[i-1:i+1] in Valid_combinations:
+                val=Roman[Roman[i]]-Roman[Roman[i-1]]
+                continue 
+            if current> val:
+                return False 
+                val=current
+            return True 
         pass
 
 
